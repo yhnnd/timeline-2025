90ac9f28e0a28a4afab69a1619088464b5ee220f
yhnnd <7866938+yhnnd@users.noreply.github.com>
2025-01-03 14:28:03 +0800

    Update wc-table.js

--- js/vendor/wc-table.js
+++ js/vendor/wc-table.js
@@ -1,143 +1,141 @@
 (function () {
 
-    function parse(s) {
+    function parse(line) {
         let ins = [], tmpout = "", tmpin = "", isOpen = false, n = 0, isQuoted = false;
         // Read all {text} in table cells.
-        for (let i = 0; i < s.length; i++) {
+        for (const char of line) {
             if (isOpen === true) {
-                if (s[i] === '"') {
+                if (char === '"') {
                     isQuoted = !isQuoted;
                 }
-                if (isQuoted === false && s[i] === "}") {
+                if (isQuoted === false && char === "}") {
                     isOpen = false;
                     ins[n] = tmpin;
                     tmpin = "";
                 }
             }
             if (isOpen === true) {
-                tmpin += s[i];
+                tmpin += char;
             } else {
-                tmpout += s[i];
-                if (s[i] === ",") {
+                tmpout += char;
+                if (char === ",") {
                     ++n;
                 }
             }
-            if (s[i] === "{" && isOpen === false) {
+            if (char === "{" && isOpen === false) {
                 isOpen = true;
                 isQuoted = false;
             }
         }
-        
-        l = tmpout.split(":").map(e => e.trim()).map(function(e) {
-            if(e.startsWith("[")) {
+
+        const layers = tmpout.split(":").map(e => e.trim()).map(function(e) {
+            if (e.startsWith("[")) {
                 e = e.replace("[", "").replace("]", "");
                 return e.split(",").map(e => e.trim());
             }
             return e;
         });
-        let root = null;
-        let a = null;
-        let node = function (q) {
-            let x = q.split("."), id = null, d = null;
+
+        const getNode = function (query) {
+            const x = query.split(".");
+            let id = null, result = null;
             for (let i = 0; i < x.length; i++) {
                 let z = x[i].split("#");
                 if (i == 0) {
-                    d = document.createElement(z[0]);
+                    result = document.createElement(z[0]);
                     if (z[0] == "script") {
-                        d.setAttribute("type", "text/javascript-unsafe");
+                        result.setAttribute("type", "text/javascript-unsafe");
                     }
                 } else {
-                    d.classList.add(z[0]);
+                    result.classList.add(z[0]);
                 }
                 if (z.length > 1) {
                     id = z[1];
                 }
             }
             if (id) {
-                d.setAttribute("id", id);
+                result.setAttribute("id", id);
             }
-            return d;
+            return result;
         };
-    
-        for (e of l) {
-            if (!a) {
-                a = node(e);
-                root = a;
+
+        let root = null;
+        let curr = null;
+        for (const e of layers) {
+            if (!curr) {
+                curr = getNode(e);
+                root = curr;
             } else if (typeof e === "string") {
-                const temp = node(e);
+                const temp = getNode(e);
                 const rand = ("" + Math.random()).split(".")[1];
                 temp.setAttribute("rand", rand);
-                a.append(temp);
-                a = a.querySelector("[rand='" + rand + "']");
-                a.removeAttribute("rand");
+                curr.append(temp);
+                curr = curr.querySelector("[rand='" + rand + "']");
+                curr.removeAttribute("rand");
             } else if (e instanceof Array) {
                 for (let i = 0; i < e.length; ++i) {
-                    p = e[i].split("=").map(u => u.trim());
-                    const temp = node(p[0]);
+                    const p = e[i].split("=").map(u => u.trim());
+                    const temp = getNode(p[0]);
                     temp.innerText = ins[i] || "";
-                    a.append(temp);
+                    curr.append(temp);
                 }
             }
         }
         return root;
     }
-    
+
     function parseTable(lines) {
-        let r = null, head = null, body = null;
-        
-        for (let i = 0; i < lines.length; ++i) {
-            let l = lines[i];
-            if (l.startsWith("table")) {
-                r = parse(l);
-            } else if (l.startsWith("thead")) {
-                head = parse(l);
-            } else if (l.startsWith("tbody")) {
-                body = parse(l);
-            } else if (l.startsWith("tr")) {
+        let root = null, head = null, body = null;
+        for (const line of lines) {
+            if (line.startsWith("table")) {
+                root = parse(line);
+            } else if (line.startsWith("thead")) {
+                head = parse(line);
+            } else if (line.startsWith("tbody")) {
+                body = parse(line);
+            } else if (line.startsWith("tr")) {
                 if (body) {
-                    body.append(parse(l));
+                    body.append(parse(line));
                 } else if (head) {
-                    head.append(parse(l));
+                    head.append(parse(line));
                 } else {
                     body = document.createElement("tbody");
-                    body.append(parse(l));
+                    body.append(parse(line));
                 }
             }
         }
-        if (!r) {
-            r = document.createElement("table");
+        if (!root) {
+            root = document.createElement("table");
         }
-        r.append(head);
-        r.append(body);
-        return r;
+        root.append(head);
+        root.append(body);
+        return root;
     }
-    
-    
+
+
     function compileTable(table) {
         if (!table) {
             return false;
         }
-    
+
         if (typeof table === "string") {
             const temp = document.createElement("div");
             temp.innerHTML = table;
             table = temp.querySelector("table");
         }
-    
+
         // Compile Table
         let tableTagName = table.tagName.toLowerCase();
         let tableClassName = table.className;
-        
         let s0 = tableTagName;
         if (tableClassName) {
             s0 += "." + tableClassName;
         }
-        
+
         // Compile THead
         const thead = table.querySelector("thead");
         const theadTr = table.querySelector("thead > tr");
         const theadTrTds = theadTr.querySelectorAll("td,th");
-    
         let s1 = thead.tagName.toLowerCase();
         if (thead.className) {
             s1 += "." + thead.className;
@@ -161,16 +159,14 @@
             }
         }
         s1 += "]";
-        
+
         // Compile TBody
         const tbody = table.querySelector("tbody");
         const tbodyTrs = table.querySelectorAll("tbody > tr");
-        
         let s2 = tbody.tagName.toLowerCase();
         if (tbody.className) {
             s2 += "." + tbody.className;
         }
-    
         let lines = [s0, s1, s2];
         // Compile TBody Tr
         for (let r = 0; r < tbodyTrs.length; ++r) {
@@ -197,13 +193,13 @@
             sr += "]";
             lines.push(sr);
         }
-        
+
         return lines;
     }
-    
-    
+
+
     function createTable() {
-        let table = `<table border="1">
+        const table = `<table border="1">
         <thead>
             <tr>
                 <th><textarea></textarea></th>
@@ -231,7 +227,7 @@
     </table>`;
         return table;
     }
-    
+
     function test1() {
         const scripts = [
             // "thead : tr : [th = {A}, th = {B}]", 
@@ -247,7 +243,7 @@
             "table": parseTable(scripts)
         };
     }
-    
+
     function test2() {
         const scripts = [
             "table",

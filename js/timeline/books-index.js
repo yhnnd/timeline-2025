5aa7d20a1d7a611770c70220e1700ae621950027
user <user@example.com>
2025-06-07 21:07:37 +0800

    up

--- js/timeline/books-index.js
+++ js/timeline/books-index.js
@@ -681,6 +681,7 @@ window.books[22].indexList = [
     "@2025/yhnnd.wordpress.com/22-diary-2025-01-2025-12-出路6/2025.04.15.txt",
     "@2025/yhnnd.wordpress.com/22-diary-2025-01-2025-12-出路6/2025.05.01.txt",
     "@2025/yhnnd.wordpress.com/22-diary-2025-01-2025-12-出路6/2025.05.26.txt",
+    "@2025/yhnnd.wordpress.com/22-diary-2025-01-2025-12-出路6/2025.06.07.txt",
 ];
 
 window.repositoryMap = {

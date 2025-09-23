cca7392ef713a763ae4ecf3a1da5ef7f2d315406
yhnnd <7866938+yhnnd@users.noreply.github.com>
2025-09-23 20:19:17 +0800

    up

--- js/timeline/books-index.js
+++ js/timeline/books-index.js
@@ -239,6 +239,8 @@ window.books[7] = {};
 window.books[7].indexList = [
     "@2023/yhnnd.wordpress.com/7-diary-2018-12-2020-05-棕書2/2018.12.18.txt",
     "@2023/yhnnd.wordpress.com/7-diary-2018-12-2020-05-棕書2/2018.12.22.txt",
+    "@2023/yhnnd.wordpress.com/7-diary-2018-12-2020-05-棕書2/2018.12.29.txt",
+    "@2023/yhnnd.wordpress.com/7-diary-2018-12-2020-05-棕書2/2019.01.05.txt",
 ];
 
 window.books[8] = {};

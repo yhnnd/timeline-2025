343fa93bd929fed6a8735e156e1e6278eaa378b0
yuhaining <yhn@a.com>
2025-12-13 15:13:45 +0800

    up

--- js/timeline/book-reader.js
+++ js/timeline/book-reader.js
@@ -111,7 +111,6 @@ if (getParameter("is-iframe") !== "true") { // Loading image and footer video in
     };
 
     document.querySelector(".loading").style.display = "flex";
-    document.querySelector(".desktop").style.minHeight = "calc(100vh - 420px)";
 }
 
 const symbols = ['⓪', '①', '②', '③', '④', '⑤', '⑥', '⑦', '⑧', '⑨', '⑩', '⑪', '⑫', '⑬', '⑭', '⑮', '⑯', '⑰', '⑱', '⑲', '⑳', '㉑', '㉒', '㉓', '㉔', '㉕', '㉖', '㉗', '㉘', '㉙', '㉚', '㉛', '㉜', '㉝', '㉞', '㉟', '㊱', '㊲', '㊳', '㊴', '㊵', '㊶', '㊷', '㊸', '㊹', '㊺', '㊻', '㊼', '㊽', '㊾', '㊿'];

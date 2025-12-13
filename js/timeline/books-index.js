d25f578e9ff55ccf37e30cce88ef09f364a53869
yuhaining <yhn@a.com>
2025-12-13 12:41:37 +0800

    up

--- js/timeline/books-index.js
+++ js/timeline/books-index.js
@@ -730,6 +730,7 @@ window.books[22].indexList = [
     "@2025/yhnnd.wordpress.com/22-diary-2025-01-2025-12-出路6/2025.11.06.txt",
     "@2025/yhnnd.wordpress.com/22-diary-2025-01-2025-12-出路6/2025.11.22.txt",
     "@2025/yhnnd.wordpress.com/22-diary-2025-01-2025-12-出路6/2025.12.03.txt",
+    "@2025/yhnnd.wordpress.com/22-diary-2025-01-2025-12-出路6/2025.12.13.txt",
 ];
 
 window.repositoryMap = {
